f755d8a6b1288a4e302e28a28b85b5d85d5f491f
Pierre Nigron <pierre.nigron@free.fr>
2022-08-17 13:11:46 +0200

    Utilisation des nouvelles tactiques SSH

--- src/CoqNom/src/Formalisation/Formats/SSH/ssh_rel.v
+++ src/CoqNom/src/Formalisation/Formats/SSH/ssh_rel.v
@@ -19,11 +19,9 @@ Proof. destruct parse_string_rel. eapply a. Qed.
 Definition parse_packet_dh_init_rel :
   { code : PHOAS (Unknown "SSHPacket") |
     forall data s, adequate (fun _ _ _ => True%type) parse_packet_dh_init code data s}.
-  eapply exist. intros. unfold parse_packet_dh_init.
-  unfold combinator.map.
-  step. eapply parse_string_adequate.
-  eapply (extern_adequate "SSHPacket" "create_DiffieHellmanInit"
-            (CONS (Var vres) NIL)).
+  eapply exist. intros. unfold parse_packet_dh_init, combinator.map.
+  repeat step. eapply parse_string_adequate.
+  eapply (extern_adequate "SSHPacket" "create_DiffieHellmanInit" (CONS (Var vres) NIL)).
 Defined.
 
 Definition parse_packet_dh_init_adequate : forall data s,
@@ -35,9 +33,7 @@ Definition parse_packet_dh_reply_rel :
   { code : PHOAS (Unknown "SSHPacket") |
     forall data s, adequate (fun _ _ _ => True%type) parse_packet_dh_reply code data s}.
   eapply exist. intros. unfold parse_packet_dh_reply.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
+  repeat step. 1 - 3 : eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_DiffieHellmanReply"
             (CONS (Var vres) (CONS (Var vres0) (CONS (Var vres1) NIL)))).
 Defined.
@@ -50,9 +46,8 @@ Definition parse_packet_debug_rel :
   { code : PHOAS (Unknown "SSHPacket") |
     forall data s, adequate (fun _ _ _ => True%type) parse_packet_debug code data s}.
   eapply exist. intros. unfold parse_packet_debug.
-  step. eapply be_u8_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
+  repeat step. eapply be_u8_adequate.
+  1-2 : eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_Debug"
             (CONS (EBin ELt (Const (ENat 0)) (EUna EVal (Var vres)))
                (CONS (Var vres0)
@@ -67,10 +62,9 @@ Proof. destruct parse_packet_debug_rel. eapply a. Qed.
 Definition parse_packet_disconnect_rel :
   { code : PHOAS (Unknown "SSHPacket") |
     forall data s, adequate (fun _ _ _ => True%type) parse_packet_disconnect code data s}.
-  eapply exist. intros. unfold parse_packet_disconnect.
-  step. eapply be_u32_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
+  eapply exist. intros. unfold parse_packet_disconnect. repeat step.
+  eapply be_u32_adequate.
+  1-2 : eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_Disconnect"
             (CONS (Var vres)
                (CONS (Var vres0)
@@ -84,20 +78,11 @@ Proof. destruct parse_packet_disconnect_rel. eapply a. Qed.
 Definition parse_packet_key_exchange_rel :
   { code : PHOAS (Unknown "SSHPacket") |
     forall data s, adequate (fun _ _ _ => True%type) parse_packet_key_exchange code data s}.
-  eapply exist. unfold parse_packet_key_exchange. intros.
-  step. unfold usize_16. step.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply parse_string_adequate.
-  step. eapply be_u8_adequate.
-  step. eapply be_u32_adequate.
+  eapply exist. unfold parse_packet_key_exchange, usize_16. intros.
+  repeat step.
+  1 - 10 : eapply parse_string_adequate.
+  eapply be_u8_adequate.
+  eapply be_u32_adequate.
   eapply (extern_adequate "SSHPacket" "create_KeyExchange"
             (CONS (Var vres)
                (CONS (Var vres0)
@@ -123,16 +108,14 @@ Proof. destruct parse_packet_key_exchange_rel. eapply a. Qed.
 Definition parse_ssh_packet_rel :
   { code : PHOAS (Pair (Unknown "SSHPacket") Span) |
     forall data s, adequate (fun _ _ _ => True%type) parse_ssh_packet code data s}.
-  eapply exist. intros. unfold parse_ssh_packet.
-  step. eapply be_u32_adequate.
-  step. eapply be_u8_adequate.
-  be_spec_clean. step.
-  step. step.
+  eapply exist. intros. unfold parse_ssh_packet. repeat step.
+  eapply be_u32_adequate.
+  eapply be_u8_adequate.
+  be_spec_clean. repeat step.
   eapply map_parser_adequate. eapply consequence_adequate. step.
-  intros. repeat clean_up. subst. repeat econstructor. auto.
-  intros. step. eapply be_u8_adequate.
-  eapply (natN_switch_adequate _ (EUna EVal (Var vres1))); repeat econstructor; eauto.
-  be_spec_clean. subst. repeat econstructor.
+  intros. repeat clean_up. finish_me.
+  intros. repeat step. eapply be_u8_adequate.
+  eapply (natN_switch_adequate _ (EUna EVal (Var vres1))); be_spec_clean; finish_me.
 
   eapply (LScons_adequate _ _ 31).
   eapply parse_packet_dh_reply_adequate.
@@ -147,26 +130,22 @@ Definition parse_ssh_packet_rel :
   eapply parse_packet_key_exchange_adequate.
 
   eapply (LScons_adequate _ _ 6).
-  unfold combinator.map.
-  step. eapply parse_string_adequate.
+  unfold combinator.map. step. eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_ServiceAccept" (CONS (Var vres2) NIL)).
 
   eapply (LScons_adequate _ _ 5).
-  unfold combinator.map.
-  step. eapply parse_string_adequate.
+  unfold combinator.map. step. eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_ServiceRequest" (CONS (Var vres2) NIL)).
 
   eapply (LScons_adequate _ _ 4).
   eapply parse_packet_debug_adequate.
 
   eapply (LScons_adequate _ _ 3).
-  unfold combinator.map.
-  step. eapply be_u32_adequate.
+  unfold combinator.map. step. eapply be_u32_adequate.
   eapply (extern_adequate "SSHPacket" "create_Unimplemented" (CONS (Var vres2) NIL)).
 
   eapply (LScons_adequate _ _ 2).
-  unfold combinator.map.
-  step. eapply parse_string_adequate.
+  unfold combinator.map. step. eapply parse_string_adequate.
   eapply (extern_adequate "SSHPacket" "create_Ignore" (CONS (Var vres2) NIL)).
 
   eapply (LScons_adequate _ _ 1).
@@ -174,16 +153,11 @@ Definition parse_ssh_packet_rel :
 
   eapply LSnil_adequate. intros elem elem_neq.
   destruct elem as [|elem]; try (exfalso; apply elem_neq; repeat constructor; done).
-  Ltac solve_default :=
-    eapply fail_adequate.
+  Ltac solve_default := eapply fail_adequate.
   solve_default.
   repeat (destruct elem; try (exfalso; apply elem_neq; repeat constructor; done);
           try solve_default).
-  step. eapply (take_verif_adequate _ _ (EUna EVal (Var vres0))).
-  be_spec_clean. subst. repeat econstructor.
-  eapply (ret_adequate _ _ _ (EBin EPair (Var vres1) (Var vres2))).
-  be_spec_clean. repeat clean_up. subst. repeat econstructor.
-  auto.
+  eapply (ret_adequate _ _ _ (EBin EPair (Var vres1) (Var vres2))); finish_me.
 Defined.
 
 Definition equiv_parse_ssh_packet_rel {var} :
