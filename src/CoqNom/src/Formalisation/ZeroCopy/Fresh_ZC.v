8020aaf99b93bed39cc8bb6d0950b90c32c807a4
Pierre Nigron <pierre.nigron@free.fr>
2022-09-14 20:04:34 +0200

    Preuve que fresh -> zéro-copie

---
+++ src/CoqNom/src/Formalisation/ZeroCopy/Fresh_ZC.v
@@ -0,0 +1,205 @@
+From Formalisation Require Import Span Inject ProgramLogic Monotone adequacy FuelMono ZC.
+Require Import Coq.Program.Equality.
+From Equations Require Import Equations.
+
+Section ZC.
+
+  Context {atom : Type}.
+
+  Lemma Fresh_ZC_aux {X} `{Foldable X}: forall e s n s_res,
+      {{ injectSL n (pos s) }} e {{ res; <absorb> all_disjointMSL res }} ->
+      forall (data : list atom) fuel  (res : X span),
+        n <= pos s ->
+        run fuel e data s = Res (s_res, res) ->
+        forall v, v ∈ M_to_list res ->
+             set_span v ⊆ inject n (pos s + len s).
+  Proof.
+    revert X H. fix IH 3. intros X H e.
+    dependent destruction e; intros; simpl in *. revert H1 H2. intros LE H1.
+    - inversion H1. subst. clear H1.
+      revert H0. MonPred.unseal. unfold monpred.monPred_wand_def. simpl.
+      unfold monpred.monPred_upclosed. simpl. intro. destruct H0. simpl in *.
+      edestruct (monPred_in_entails tt). clear monPred_in_entails.
+      instantiate (1 := ∅). MonPred.unseal.
+      split. split. inversion_star h P. clear H0.
+      red in P1. destruct P1. inversion H1. subst. clear H1. clear P2.
+      rewrite union_empty_r_L in P. clear monPred_in_entails. subst.
+      edestruct H0.
+      + eexists ∅, _. repeat split; auto. 2 : eapply disjoint_empty_l.
+        eapply SepSet.soundness. iIntros "HA". iApply big_op_ctx. iApply "HA".
+      + destruct H1 as [h [P3 [P1 [P2 P4]]]]. clear H0 P0 P3.
+        rewrite union_empty_l_L in P4.
+        transitivity (inject n (Span.pos s_res)).
+        2 : { eapply inject_mono_r. lia. }
+        rewrite P4. transitivity h. 2 : set_solver. clear P2 P4. unfold all_disjointMSL in P1.
+        revert h P1. induction (M_to_list res); inversion H3; subst; intros.
+        * clear IHl. unfold set_span. unfold all_disjointSL in P1. simpl in *.
+          eapply (monPred_at_sep tt (IsFresh a)) in P1.
+          unfold hpropI in P1. simpl in *.
+          unfold bi_sep in P1. inversion_star h P. clear P1 P2 P3.
+          unfold IsFresh in P0. destruct a. simpl in *. rewrite P. clear P H3.
+          revert pos h0 P0. induction len using N.peano_ind; intros.
+          -- rewrite inject_empty. 2 : lia. set_solver.
+          -- rewrite <- N.succ_pos_spec in P0.
+             rewrite IsFresh_aux_equation_2 in P0.
+             rewrite N.succ_pos_spec in P0. rewrite N.pred_succ in P0.
+             eapply (monPred_at_sep tt (& pos)) in P0.
+             unfold hpropI in P0. simpl in *.
+             unfold bi_sep in P0. inversion_star h P. clear P0.
+             inversion P1. subst. clear P1. eapply IHlen in P2.
+             rewrite inject_aux_add_head.
+             assert (N.succ pos + len = pos + N.succ len) by lia. rewrite <- H0.
+             set_solver. lia.
+        * unfold all_disjointSL in P1. simpl in *.
+          eapply (monPred_at_sep tt (IsFresh a)) in P1.
+          unfold hpropI in P1. simpl in *.
+          unfold bi_sep in P1. inversion_star h P. clear P1.
+          rewrite P. transitivity h1. 2 : set_solver.
+          eapply IHl; auto.
+    - destruct n; simpl in *.
+      + inversion H2.
+      + unfold_MonSem. eapply IH; eauto.
+        iIntros "HA". iDestruct (H0 with "HA") as "HB". iApply "HB".
+      + unfold_MonSem. destruct (run_read s0 n data s) as [[s_i i]| |] eqn:?.
+        eapply run_read_eq_span in Heqr. subst.
+        eapply IH; eauto.
+        iIntros "HA". iDestruct (H0 with "HA") as "HB". iApply "HB".
+        inversion H2. inversion H2.
+      + unfold set_span.
+        unfold_MonSem. unfold run_take in H2. unfold_MonSem.
+        destruct (n <=? len s) eqn:?.
+        * pose (mono := H2). eapply run_mono in mono as [P0 P1]. simpl in *.
+          eapply N.leb_le in Heqb. assert (pos s + len s = pos s + n + (len s - n)) by lia.
+          rewrite H4.
+          unfold set_span in IH.
+          eapply (IH _ _ _ {| pos := pos s + n; len := len s - n |}); simpl in *; eauto.
+          2 : lia.
+          iIntros "HA". unfold injectSL. rewrite (inject_add n n1 (pos s)). 2 : lia.
+          iDestruct (big_sepS_union with "HA") as "[HA HB]". eapply inject_disjoint.
+          iDestruct (inject_IsFresh with "[HB]") as "HB"; eauto.
+          unfold injectSL. instantiate (1 := (mk_span (pos s) n)). simpl. iApply "HB".
+          iApply (H0 with "HA HB").
+        * inversion H2.
+      + unfold set_span. unfold_MonSem.
+        destruct (run fuel n data s) as [[s_t t]| |] eqn:?.
+        * pose (mono_n := Heqr). eapply run_mono in mono_n as [P0 P1].
+          transitivity (inject n1 (pos s_t + len s_t)).
+          2 : { eapply inject_mono_r. lia. }
+          eapply IH. 3 : eapply H2. 2 : lia. 2 : auto.
+          iIntros "HA". unfold injectSL.
+          rewrite (inject_union (pos s) n1). 2-3 : lia.
+          iDestruct (big_sepS_union with "HA") as "[HA HB]". eapply inject_disjoint.
+          iDestruct (H0 with "HA") as "[HA _]".
+          destruct (soundness X0 n data fuel).
+          iDestruct (sound with "[HA] [HB]") as "HA". eapply Heqr.
+          iApply (wp_consequence with "HA"). iIntros (v0) "HA". iModIntro. iApply "HA".
+          auto. simpl. iApply (wp_absorb_out with "HA").
+        * destruct (run fuel n2 data s) as [[s_t t]| |] eqn:?.
+          -- pose (mono_n2 := Heqr0). eapply run_mono in mono_n2 as [P0 P1].
+             transitivity (inject n1 (pos s_t + len s_t)).
+             2 : { eapply inject_mono_r. lia. }
+             eapply IH. 3 : eapply H2. 2 : lia. 2 : auto.
+             iIntros "HA". unfold injectSL.
+             rewrite (inject_union (pos s) n1). 2-3 : lia.
+             iDestruct (big_sepS_union with "HA") as "[HA HB]". eapply inject_disjoint.
+             iDestruct (H0 with "HA") as "[_ HA]".
+             destruct (soundness X0 n2 data fuel).
+             iDestruct (sound with "[HA] [HB]") as "HA". eapply Heqr0.
+             iApply (wp_consequence with "HA"). iIntros (v0) "HA". iModIntro. iApply "HA".
+             auto. simpl. iApply (wp_absorb_out with "HA").
+          -- inversion H2.
+          -- inversion H2.
+        * inversion H2.
+      + unfold_MonSem. destruct o.
+        * destruct (run fuel n data s0) as [[s_t t]| |]eqn:?.
+          -- eapply IH. 2 : lia. 2 : eapply H2. 2 : auto.
+             iIntros "HA". iDestruct (H0 with "HA") as "[HA HB]".
+             iDestruct (IsFresh_injectSL with "HA") as "HA".
+             iApply wp_absorb_out.
+             pose (mono_n := Heqr). eapply run_mono in mono_n as [P0 P1].
+             unfold injectSL. rewrite (inject_union (pos s_t) (pos s0)).
+             iDestruct (big_sepS_union with "HA") as "[HA _]". eapply inject_disjoint.
+             destruct (soundness X0 n data fuel).
+             iDestruct (sound with "[HB] HA") as "HB"; eauto.
+             iApply (wp_consequence with "HB"). auto. auto. lia. lia.
+          -- inversion H2.
+          -- inversion H2.
+        * destruct (run fuel n data s) as [[s_t t]| |]eqn:?.
+          -- eapply IH. 2 : lia. 2 : eapply H2. 2 : auto.
+             iIntros "HA". iApply (H0 with "HA").
+          -- inversion H2.
+          -- inversion H2.
+      + destruct o; unfold_MonSem.
+        * revert x s H0 H1 H2.  induction (N.to_nat n2).
+          -- intros. eapply IH. 2 : lia. 2 : eapply H2. 2 : auto.
+             iIntros "HA". iDestruct (H0 with "HA") as (Q) "[HA [HB HC]]".
+             iApply wp_absorb_out. iClear "HB". iModIntro. iApply ("HC" with "HA").
+          -- intros. destruct (run fuel (n x) data s) as [[s_t t]| |] eqn:?.
+             ++ pose (mono := Heqr). eapply run_mono in mono as [P0 P1].
+                transitivity (inject n1 (pos s_t + len s_t)).
+                2 : { eapply inject_mono_r. lia. }
+                eapply IHn3 in H2. auto.
+                iIntros "HA". unfold injectSL.
+                rewrite (inject_union (pos s) n1). 2-3 : lia.
+                iDestruct (big_sepS_union with "HA") as "[HA HB]". eapply inject_disjoint.
+                iDestruct (H0 with "HA") as (Q) "[HA [#HD HC]]".
+                iExists Q. iDestruct ("HD" with "HA") as "HA".
+                destruct (soundness X0 (n x) data fuel).
+                iDestruct (sound with "[HA] HB") as "HA"; eauto.
+                iApply (wp_consequence with "HA"). iIntros (v0) "HA". iApply "HA".
+                iDestruct "HA" as ">HA". iFrame. auto. lia.
+             ++ inversion H2.
+             ++ inversion H2.
+        * assert (exists fuel0, fuel0 = fuel). exists fuel. reflexivity.
+          destruct H4. rewrite <- H4 in H2 at 1. clear H4. revert x s H0 H1 H2. induction x0.
+          -- intros. inversion H2.
+          -- intros. destruct (run fuel (n x) data s) as [[s_t t] | | ] eqn:?.
+             ++ pose (mono := Heqr). eapply run_mono in mono as [P0 P1].
+                transitivity (inject n1 (pos s_t + len s_t)).
+                2 : { eapply inject_mono_r. lia. }
+                eapply IHx0. instantiate (1 := t).
+                iIntros "HA". unfold injectSL.
+                rewrite (inject_union (pos s) n1). 2-3 : lia.
+                iDestruct (big_sepS_union with "HA") as "[HA HB]". eapply inject_disjoint.
+                iDestruct (H0 with "HA") as (Q) "[HA [#HD HC]]".
+                iExists Q. iFrame. destruct (soundness X0 (n x) data fuel).
+                iDestruct ("HD" with "HA") as "HA".
+                iDestruct (sound with "[HA] HB") as "HA"; eauto.
+                iApply (wp_consequence with "HA"). iIntros (v0) "HA". iApply "HA".
+                iDestruct "HA" as ">HA". iFrame. auto. lia.
+                destruct ((fix sem_repeat_none (n2 : nat) (x0 : X0) {struct n2} : MonSem X0 :=
+                            match n2 with
+                            | 0%nat => λ _ : span, NoFuel
+                            | S n3 =>
+                                λ s0 : span,
+                                  match
+                                    match run fuel (n x0) data s0 with
+                                    | Res (s1, x2) => sem_repeat_none n3 x2 s1
+                                    | NoRes => NoRes
+                                    | NoFuel => NoFuel
+                                    end
+                                  with
+                                  | Res (s1, v0) => Res (s1, v0)
+                                  | NoRes => Res (s0, x0)
+                                  | NoFuel => NoFuel
+                                  end
+                            end) x0 t s_t) as [[s_b b]| |] eqn:?; auto.
+                eapply no_NoRes in Heqr0. contradiction.
+             ++ eapply IH. 2 : auto. 2 : apply H2. 2 : auto.
+                iIntros "HA". iDestruct (H0 with "HA") as (Q) "[HA [HB HC]]".
+                iApply wp_absorb_out. iClear "HB". iModIntro. iApply ("HC" with "HA").
+             ++ inversion H2.
+  Qed.
+
+  Lemma Fresh_ZC {X} `{Foldable X}: forall e s s_res v,
+      {{ emp }} e {{ res; <absorb> all_disjointMSL res }} ->
+      forall (data : list atom) fuel (res : X span),
+        run fuel e data s = Res (s_res, res) ->
+          v ∈ M_to_list res ->
+          scope_in v s.
+  Proof.
+    intros. eapply Fresh_ZC_aux. iIntros "HA". iApply H0.
+    iApply (injectSL_emp with "HA"). lia. lia. eauto. auto.
+  Qed.
+
+End ZC.
---
+++ src/CoqNom/src/Formalisation/ZeroCopy/ZC.v
@@ -0,0 +1,111 @@
+From stdpp Require Import numbers countable.
+From Formalisation Require Import Span SizeNat Inject IsFresh.
+From Classes Require Import Foldable.
+Require Import Vector.
+From Equations Require Import Equations.
+Import disjoint.
+
+Definition Bit := bool.
+
+Class ValuesFormat (X : Type) :=
+  mk_values {
+      size : nat;
+      encode : X -> Vector.t Bit size;
+      decode : Vector.t Bit size -> option X;
+      spec : forall (x : X), decode (encode x) = Some x; }.
+
+Local Instance ValuesBool : ValuesFormat bool.
+refine (mk_values bool 1
+          (fun b => cons b nil)
+          (fun v => match v with
+                 | cons b _ => Some b
+                 | _ => None
+                 end) _).
+intros. reflexivity.
+Defined.
+
+Class Etiquette `{Countable etiquette} :=
+  mk_etiquette {
+      set_etiquette : gset etiquette;
+      set_etiquette_spec: forall (e : etiquette), e ∈ set_etiquette; }.
+
+Inductive Result :=
+| Value : forall `{ValuesFormat X}, X -> Result
+| Span : span -> Result
+| Struct : forall `{Etiquette etiquette}, (etiquette -> Result) -> Result.
+
+Arguments Value [X _].
+Arguments Struct [etiquette _ _ _].
+
+Definition list_etiquette {X} `{Etiquette X} := elements set_etiquette.
+
+Fixpoint Result_to_list (t: Result) : list span :=
+  match t with
+  | Value _ => []
+  | Span s => [s]
+  | Struct st =>
+      list.foldr (fun eti r => Result_to_list (st eti) ++ r) [] list_etiquette
+  end.
+
+Definition Decodeur := span -> option Result.
+
+Open Scope N_scope.
+
+Definition set_span (s : span) := inject (pos s) (pos s + len s).
+
+Definition scope_in (s t : span) := set_span s ⊆ set_span t.
+
+Fixpoint ResultWeakZC (s : span) (r : Result) : Prop :=
+  match r with
+  | Value _ => True
+  | Span v => scope_in v s
+  | Struct ft => forall e, ResultWeakZC s (ft e)
+  end.
+
+Definition DecodeurWeakZC (d: Decodeur) :=
+  forall s ft,
+    d s = Some ft ->
+    ResultWeakZC s ft.
+
+Fixpoint ResultZC (s : span) (r : Result) : Prop :=
+  match r with
+  | Value _ => False
+  | Span v => scope_in v s
+  | Struct ft => forall e, ResultZC s (ft e)
+  end.
+
+Definition DecodeurZC (d : Decodeur) :=
+  forall s ft,
+    d s = Some ft ->
+    ResultZC s ft.
+
+(** Version tous les spans de la structures sont disjointes deux à deux **)
+
+Definition Result_safe (r : Result) : Prop :=
+  forall s t, s <> t -> s ∈ Result_to_list r -> t ∈ Result_to_list r -> disjoint s t.
+
+(** Version SL **)
+
+Definition Result_safeSL (r : Result) : iProp :=
+  [∗ list] v ∈ Result_to_list r, IsFresh v.
+
+Theorem safe_bridge : forall (r : Result), Result_safeSL r ⊢ ⌜ Result_safe r ⌝.
+Proof.
+  unfold Result_safe. induction r; simpl; intros.
+  - iIntros "HA". iPureIntro. intros s t NEQ F. inversion F.
+  - iIntros "HA". iPureIntro. intros t0 t1 NEQ INt0 INt1.
+    eapply elem_of_list_singleton in INt0. eapply elem_of_list_singleton in INt1.
+    subst. contradiction.
+  - iIntros "HA" (s t NEQ INs INt).
+    unfold Result_safeSL. simpl.
+    eapply elem_of_list_lookup_1 in INs as [Is Ps].
+    eapply elem_of_list_lookup_1 in INt as [It Pt].
+    iDestruct (big_sepL_delete with "HA") as "[Hs HA]". eapply Ps.
+    iDestruct (big_sepL_delete with "HA") as "[Ht HA]". eapply Pt.
+    destruct (decide (It = Is)).
+    + subst. rewrite Ps in Pt. injection Pt. intro. contradiction.
+    + iClear "HA". iApply (IsFresh_spec with "Hs Ht").
+Qed.
+
+Definition DecodeurZC_safe (d : Decodeur) :=
+  forall s ft, d s = Some ft -> Result_safe ft.
