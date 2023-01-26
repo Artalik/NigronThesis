c1bd6d785ee4f80e5c3bcfe183eac19742f51ecb
Pierre Nigron <pierre.nigron@free.fr>
2023-01-26 15:37:04 +0100

    Ajout bench Rust SSH

---
+++ src/CoqNom/src/Extraction/C/SSHC/main.rs
@@ -0,0 +1,25 @@
+extern crate ssh_parser;
+use ssh_parser::*;
+use std::time::{Instant};
+
+static IKEV2_INIT_RESP: &'static [u8] = include_bytes!("../assets/dh_reply.raw");
+
+fn main() {
+    let bytes = IKEV2_INIT_RESP;
+    let mut res = parse_ssh_packet(&bytes);
+    for _i in 0..100000 {
+        res = parse_ssh_packet(&bytes);
+    }
+    let ite = 10;
+    let mut all = 0;
+    for _j in 0..ite {
+        let now = Instant::now();
+        for _i in 0..100000000 {
+            res = parse_ssh_packet(&bytes);
+        }
+        let elapsed_time = now.elapsed();
+        all = all + elapsed_time.as_millis();
+    }
+    println!("Temps moyen pour 100 000 000 parsing :  {} ms.", all / ite);
+    println!("{:?}",res);
+}
